1b48e4ac9497a745d9623ac6c5e7e4c500146b96
Arnau Manyosa-Plaza <arnau.manyosa@protonmail.com>
2021-12-02 23:12:37 +0100

    Create install-zsh.sh script

---
+++ install-zsh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Installs .oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  # Installs Oh my ZSH with Homebrew
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Assumes default ZSH installation
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+# Installs plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Fix permissions
+chmod 700 ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
