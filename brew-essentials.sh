237db84d205e904d59280d7a723448c978ceac8d
Arnau Manyosa-Plaza <arnau.manyosa@protonmail.com>
2025-11-21 10:15:20 +0100

    Clean and Update brew-essentials.sh

--- brew-essentials.sh
+++ brew-essentials.sh
@@ -22,39 +22,35 @@ brew install node
 brew install docker
 brew install alfred
 brew install mackup
-# brew install dropbox
 brew install google-chrome #Chrome
-# brew install google-drive
-# brew install homebrew/cask-versions/firefox-nightly # Nightly
-# brew install homebrew/cask-versions/google-chrome-canary # Chrome Canary
 brew install --cask firefox
 brew install --cask brave-browser
 # brew install iterm2
 brew install kap
-# brew install mongodb-compass
-# brew install notion
-# brew install signal
 # brew install slack
 brew install spotify
-# brew install textexpander
-# brew install tower
 brew install visual-studio-code
 brew install whatsapp
 # brew install zoom
 brew install --cask telegram
-brew install --cask macs-fan-control
+# brew install --cask macs-fan-control
 brew install --cask deepl
 brew install --cask dupeguru
+brew install czkawka 
 brew install --cask handbrake
 brew install --cask figma
 brew install --cask postman
-brew install --cask skype
+brew install --cask microsoft-teams
 brew install --cask the-unarchiver
 brew install --cask veracrypt
 brew install --cask vlc
 brew install --cask appcleaner
 # brew install --cask freetube
+brew install --cask grayjay
 brew install --cask rectangle
+brew install --cask warp
+brew install --cask topnotch
+brew install --cask commander-one
 
 # Restore plists of apps
 mackup restore
